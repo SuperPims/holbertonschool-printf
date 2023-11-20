81dc9cb3faaa5d2dcbdf41cdd12335c1091636dc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-20 20:19:59 +0100

    printd_int  complete rework

--- printd_int.c
+++ printd_int.c
@@ -2,7 +2,35 @@
 
 int printd_int(va_list args)
 {
-	int intd = va_arg(args, int);
-	puts(intd);
-	return (strlen(intd + 1));
+	int intd = va_arg(args, int), temp, intlength = 0, i = 0;
+	char *intstring;
+
+	if (intd < 0)
+	{
+		putchar('-');
+		intd = -intd;
+	}
+	temp = intd;
+	while (temp != 0)
+	{
+		temp = temp / 10;
+		intlength++;
+	}
+	intstring = malloc((intlength + 1) * sizeof(char));
+	if (intstring == NULL)
+	{
+		return 0;
+	}
+
+	itoa(intd, intstring, 10);
+
+	while (intstring[i] != '\0')
+	{
+		putchar(intstring[i]);
+		i++;
+	}
+	
+	free(intstring)
+	
+	return (intlength);
 }
