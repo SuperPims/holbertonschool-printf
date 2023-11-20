62ad2b9bd393387a1959f08d7bd97791115f4329
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-20 14:42:51 +0100

    _printf search the struct to call the right function

--- _printf.c
+++ _printf.c
@@ -4,36 +4,43 @@
  * _printf - print the data passed as arguments
  * @format: character string that contain zero or more directive
  * for the output
- * Return: (0) sucess
+ * Return: int, number of printed char
  */
 
 int _printf(const char *format, ...)
 {
-    /*format_list formats[] = {
+    format_list formats[] = {
         {'c', print_char},
         {'s', print_string},
         {'%', print_modulo},
         {'i', printi_int},
         {'d', printd_int},
         {'\0', NULL}
-    };*/
+    };
 
-    int i = 0/*, j = 0*/;
-    /*va_list formatlist;
+    int i = 0, j = 0;
+    va_list formatlist;
 
     va_start(formatlist, format);*/
     while (format != NULL && format[i] != '\0')
     {
+        j = 0;
         if (format[i] == '%')
         {
-            return(1); /*temporary*/
+            while (formats[j].charac != '\0')
+            {
+                if (format[i] == formats[j].charac)
+                {
+                    formats[j].func(formatlist);
+                }
+                j++;
+            }
         }
         else
         {
             putchar(format[i]);
-     
         }
-    i++;
+        i++;
     }
     return (0); /*temporary*/
 }
