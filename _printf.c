a57c7778dc95032f1c84d3a92c85c3ebf98b653a
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-20 11:01:16 +0100

    created _printf, added comments

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - print the data passed as arguments
+ * @format: character string that contain zero or more directive
+ * for the output
+ * Return: (0) sucess
+ */
+
+int _printf(const char *format, ...)
+{
+    
+}
