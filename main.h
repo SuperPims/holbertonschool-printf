0da15022259d747dc509348b4ec980e916588f60
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 16:15:51 +0100

    betty main.h

--- main.h
+++ main.h
@@ -7,6 +7,13 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * struc formats - struct that store differents format
+ * that _printf can print and their respectives function
+ * 
+ * @charac: the char that tells wich type is to be printed
+ * @func: the function that will print the type associated
+*/
 typedef struct formats
 {
 	char charac;
