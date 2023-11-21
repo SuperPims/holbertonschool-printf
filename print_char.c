e76179fac219d98acfbd68edec9c77b51289fade
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 16:16:03 +0100

    betty print_char

--- print_char.c
+++ print_char.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
-/*define the printf_putchar function*/
+/**
+ * print_char - print a char
+ * @args: Argument list containing the character to print
+ * Return: 0 (sucess)
+ */
+
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	putchar(c);
 	return (0);
 }
