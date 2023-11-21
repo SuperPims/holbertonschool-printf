cf45f0faac2fd2f146c796dc39ee6dc86381fc07
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 16:16:13 +0100

    betty print_modulo

--- print_modulo.c
+++ print_modulo.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
-/*define the printf_putchar function*/
+/**
+ * print_modulo - print a modulo
+ * @args: Argument list containing the integer to print
+ * Return: Number of character printed
+ */
+
 int print_modulo(va_list args)
 {
 	(void)args;
 	putchar('%');
-	return 1;
+	return (1);
 }
