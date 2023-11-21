1e9b9043aaa389baf4eaa0de41ec36be0140102e
SuperPims <indexes-benzine0p@icloud.com>
2023-11-21 13:43:14 +0100

    Use tabs instead of spaces

--- printi_int.c
+++ printi_int.c
@@ -7,48 +7,47 @@
  */
 int printi_int(va_list args)
 {
-    int num = va_arg(args, int);
-    int temp, intlength = 0, i = 0;
-    char *intstring;
+	int num = va_arg(args, int);
+	int temp, intlength = 0, i = 0;
+	char *intstring;
 
-    if (num < 0)
-    {
-        putchar('-');
-        num = -num;
-    }
+	if (num < 0)
+	{
+		putchar('-');
+		num = -num;
+	}
 
-    temp = num;
-    while (temp != 0)
-    {
-        temp = temp / 10;
-        intlength++;
-    }
+	temp = num;
+	while (temp != 0)
+	{
+		temp = temp / 10;
+		intlength++;
+	}
 
-    intstring = malloc((intlength + 1) * sizeof(char));
-    if (intstring == NULL)
-    {
-        return 0;
-    }
+	intstring = malloc((intlength + 1) * sizeof(char));
+	if (intstring == NULL)
+	{
+		return 0;
+	}
 
-    // Convert integer to string directly
-    i = intlength - 1;
-    while (num != 0)
-    {
-        intstring[i] = (num % 10) + '0';
-        num = num / 10;
-        i--;
-    }
-    intstring[intlength] = '\0';
+	i = intlength - 1;
+	while (num != 0)
+	{
+		intstring[i] = (num % 10) + '0';
+		num = num / 10;
+		i--;
+	}
+	intstring[intlength] = '\0';
 
-    // Print the string
-    i = 0;
-    while (intstring[i] != '\0')
-    {
-        putchar(intstring[i]);
-        i++;
-    }
 
-    free(intstring);
+	i = 0;
+	while (intstring[i] != '\0')
+	{
+		putchar(intstring[i]);
+		i++;
+	}
 
-    return intlength;
+	free(intstring);
+
+	return intlength;
 }
