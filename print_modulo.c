3b5cc89316ab2c5de6ec3f7fc924daabe2188f47
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 10:45:42 +0100

    print_modulo commenting for testint purposes

--- print_modulo.c
+++ print_modulo.c
@@ -3,7 +3,7 @@
 /*define the printf_putchar function*/
 int print_modulo(va_list args)
 {
-	int percent = va_arg(args, int);
-	/*puts(percent);*/
+	/*int percent = va_arg(args, int);
+	puts(percent);*/
 	return (1);
 }
