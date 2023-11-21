6f79af2cdda6b522186a77bdb57456826194a777
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 16:16:24 +0100

    betty print_string

--- print_string.c
+++ print_string.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
-/*define the printf_putchar function*/
+/**
+ * print_string - print a string
+ * @args: Argument list containing the string to print
+ * Return: Number of character printed
+ */
 int print_string(va_list args)
 {
-	 char *str = va_arg(args, char *);
-    fputs(str, stdout);
-    return strlen(str);
-}	
+	char *str = va_arg(args, char *);
+	fputs(str, stdout);
+	return (strlen(str));
+}
