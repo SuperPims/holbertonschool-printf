98f4878166a0c8e458be25ef73342ef29fa3bdbf
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-21 09:48:43 +0100

    printd_int added custom itoa

--- printd_int.c
+++ printd_int.c
@@ -1,6 +1,37 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *_itoa(int num, char str[], int base)
+{
+	int i = 0, remaining = 0, start = 0, end = 0;
+	char temp;
+
+	if (num == 0)
+	{
+		str[i] = '0';
+		i++;
+		str[i] = '\0';
+		return str;
+	}
+	while (num != 0)
+	{
+		remaining = num % base;
+		str[i] = (remaining > 9) ? (remaining - 10) + 'a' : remaining + '0';
+		i++;
+		num = num / base;
+	}
+	str[i] = '\0';
+	end = i - 1;
+	while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+	return (str);
+
+}
 int printd_int(va_list args)
 {
 	int intd = va_arg(args, int), temp, intlength = 0, i = 0;
@@ -23,7 +54,7 @@ int printd_int(va_list args)
 		return 0;
 	}
 
-	itoa(intd, intstring, 10);
+	_itoa(intd, intstring, 10);
 
 	while (intstring[i] != '\0')
 	{
@@ -35,3 +66,4 @@ int printd_int(va_list args)
 	
 	return (intlength);
 }
+
