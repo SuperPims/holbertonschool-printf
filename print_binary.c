cdc46d261b54049cb3d2894dfe184e70f80c0165
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-24 15:41:05 +0100

    fix comment on print_binary

--- print_binary.c
+++ print_binary.c
@@ -3,7 +3,7 @@
 /**
  * print_binary - print an unsigned int into binary
  * @args: Argument list containing the unsigned int to print
- * Return the number of character printed
+ * Return: the number of character printed
  */
 
 int print_binary(va_list args)
