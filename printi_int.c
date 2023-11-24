f1d4ea346a1ef31107a383509093689b5b6017c1
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-11-24 17:11:41 +0100

    printd/i_int testing long int

--- printi_int.c
+++ printi_int.c
@@ -8,34 +8,5 @@
  */
 int printi_int(va_list args)
 {
-	int length = 0;
-	int divisor = 1;
-
-	int value = va_arg(args, int);
-
-	if (value == 0)
-	{
-		return (_putchar('0'));
-	}
-	if (value < 0)
-	{
-		_putchar('-');
-		length++;
-		value = -value;
-	}
-
-	while (value / divisor > 9)
-	{
-		divisor *= 10;
-	}
-
-	while (divisor >= 1)
-	{
-		_putchar('0' + value / divisor);
-		length++;
-		value %= divisor;
-		divisor /= 10;
-	}
-
-	return (length);
+	return (printd_int(args));
 }
